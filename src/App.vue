57a3261fa9475446fb719e1066342540086c00dd
i-mnam <8linkbv@gmail.com>
2021-04-26 00:06:51 +0900

    ch 2.6. router-link 장점 및 navigation bar component를 팩토링 해봄

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-    여기서부터 코드를 시작합니다!
+    <!-- <a href="/">Home</a>
+    <a href="/login">Login</a> -->
+    <!-- <router-link to="/">Home</router-link>
+    <router-link to="/login">Login</router-link> -->
+    <Navbar />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue";
+
 export default {
   name: "app",
+  components: {
+    Navbar,
+  },
   data() {
     return {};
   },
