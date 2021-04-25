8b63b5b243bd13fdc1ee680437df5808d3d42788
i-mnam <8linkbv@gmail.com>
2021-04-25 19:57:34 +0900

    ch 2.2. 라우터 vue로 직접 만들기

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+const Login = { template: '<div>Login Page</div>' }
+
+// routing map (js 객체로)
+const routes = {
+  '/': App,
+  '/login': Login,
+}
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  computed: {
+    VueComponent() {
+      return routes[window.location.pathname] || { template: '<div>Not Found</div>' }
+    }
+  },
+  // 인자로 넘어온 h()함수를 통해서 App이라는 컴포를 그림
+  // render: h => h(App) 
+  render(h) {
+    // computed 속성을 h 함수 인자로 줌
+    return h(this.VueComponent)
+  }
 })
