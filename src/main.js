f3495f6e2c6f7312bd0e9273fb48b1df77adef1f
i-mnam <8linkbv@gmail.com>
2021-04-25 21:18:39 +0900

    ch 2.3. vue-router 모듈 사용 시작

--- src/main.js
+++ src/main.js
@@ -1,25 +1,32 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
-const Login = { template: '<div>Login Page</div>' }
+// 0. 모듈 시스템을 이용 중이므로, import 모듈, 모듈 사용 설정
+Vue.use(VueRouter)
 
-// routing map (js 객체로)
-const routes = {
-  '/': App,
-  '/login': Login,
-}
+// 1. 라우트 컴포넌트를 정의
+const Login = { template: '<div>Login Page</div>' }
+const NotFound = { template: '<div>Not Found Page</div>' }
+
+// 2. 라우트 정의
+// const routes = [
+//   { path: '/', component: App },
+//   { path: '/login', component: Login },
+// ]
+
+// 3. routes 옵션을 이 곳에서 설정하면서 router 인스턴스를 만듦.
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: App },
+    { path: '/login', component: Login },
+    { path: '/*', component: NotFound },
+  ]
+});
 
 new Vue({
   el: '#app',
-  computed: {
-    VueComponent() {
-      return routes[window.location.pathname] || { template: '<div>Not Found</div>' }
-    }
-  },
-  // 인자로 넘어온 h()함수를 통해서 App이라는 컴포를 그림
-  // render: h => h(App) 
-  render(h) {
-    // computed 속성을 h 함수 인자로 줌
-    return h(this.VueComponent)
-  }
+  router,
+  render: h => h({ template: '<router-view></router-view>' })
 })
