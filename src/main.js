03a8dd1e0c6f4548579978e2da6e70ce68eeb66f
i-mnam <8linkbv@gmail.com>
2021-04-25 23:51:01 +0900

    ch 2.5. router-view를 활용한 화면 구조 처음 잡음

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-
 import router from './router' // ./router/index.js
+import App from './App.vue'
 
 new Vue({
   el: '#app',
   router,
-  render: h => h({ template: '<router-view></router-view>' })
+  render: h => h(App),// render: h => h({ template: '<router-view></router-view>' })
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../components/Home.vue'
+import Login from '../components/Login.vue'
+import NotFound from '../components/NotFound.vue'
 
-import App from '../App.vue'
-// 0. 모듈 시스템을 이용 중이므로, import lib, 라이브러리 사용 설정
+// 0. lib 사용 설정
 Vue.use(VueRouter)
-
-// 1. 라우트 컴포넌트를 정의
-const Login = { template: '<div>Login Page</div>' }
-const NotFound = { template: '<div>Not Found Page</div>' }
 
 // 3. routes 옵션을 이 곳에서 설정하면서 router 인스턴스를 만듦.
 const router = new VueRouter({
@@ -15,7 +13,7 @@ const router = new VueRouter({
     mode: 'history',
     // 2. 라우트 정의
     routes: [
-        { path: '/', component: App },
+        { path: '/', component: Home },
         { path: '/login', component: Login },
         { path: '/*', component: NotFound },
     ]
