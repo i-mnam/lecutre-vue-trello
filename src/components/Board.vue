a9e422bb60e1ea1eb3b7d184367b9b4317a9160f
i-mnam <8linkbv@gmail.com>
2021-04-26 22:35:11 +0900

    ch 2.8. 중첩라우트, 활용

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,6 +2,10 @@
   <div>
     Board
     <div>bid: {{ bid }}</div>
+    <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
+    <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+    <hr />
+    <router-view></router-view>
   </div>
 </template>
 <script>
@@ -12,7 +16,6 @@ export default {
     };
   },
   created() {
-    console.log(this.$route.params.bid);
     this.bid = this.$route.params.bid;
   },
 };
---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Card
+    <div>cid: {{ cid }}</div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      cid: 0,
+    };
+  },
+  watch: {
+    $route() {
+      this.cid = this.$route.params.cid;
+    },
+  },
+  created() {
+    console.log(this.$route.params);
+    this.cid = this.$route.params.cid;
+  },
+};
+</script>
