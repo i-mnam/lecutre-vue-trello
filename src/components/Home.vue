c8f7f2c771db870c62cb7c443fd3c20f9501e5fd
i-mnam <8linkbv@gmail.com>
2021-04-28 00:40:34 +0900

    ch 3.3 API 호출 : XMLHttpRequest객체 사용 > axios library 로 넘어감

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,13 @@
     <div>
       Board List:
       <div v-if="loading">Loading...</div>
-      <div v-else>Api result: {{ apiRes }}</div>
+      <div v-else>
+        Api result:
+        <pre>{{ apiRes }}</pre>
+        <div v-if="error">
+          <pre>{{ error }}</pre>
+        </div>
+      </div>
       <ul>
         <li><router-link to="/b/1">Board 1</router-link></li>
         <li><router-link to="/b/2">Board 2</router-link></li>
@@ -13,11 +19,14 @@
   </div>
 </template>
 <script>
+import axios from "axios"
+
 export default {
   data() {
     return {
       loading: true,
       apiRes: "",
+      error: "",
     }
   },
   created() {
@@ -27,20 +36,33 @@ export default {
     fetchData() {
       this.loading = true
 
-      const req = new XMLHttpRequest()
+      axios
+        .get("http://localhost:3000/h1ealth") // axios는 promise를 반환해서 바로 then chain을 사용할 수 있다
+        .then((res) => {
+          this.apiRes = res.data
+        })
+        .catch((res) => {
+          this.error = res.response.data
+        })
+        .finally(() => {
+          this.loading = false
+        })
 
-      req.open("GET", "http://localhost:3000/health")
+      //** XMLHttpRequest 객체 사용한 것 **//
+      // const req = new XMLHttpRequest()
 
-      req.send()
+      // req.open("GET", "http://localhost:3000/health")
 
-      req.addEventListener("load", () => {
-        this.loading = false
-        this.apiRes = {
-          status: req.status,
-          statusText: req.statusText,
-          response: JSON.parse(req.response),
-        }
-      })
+      // req.send()
+
+      // req.addEventListener("load", () => {
+      //   this.loading = false
+      //   this.apiRes = {
+      //     status: req.status,
+      //     statusText: req.statusText,
+      //     response: JSON.parse(req.response),
+      //   }
+      // })
     },
   },
 }
