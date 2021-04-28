2f1feda752393d80fcbf492707befb14b323ce60
i-mnam <in.kang@okestro.com>
2021-04-28 11:24:10 +0900

    4.2 vue-router의 navigation guard로 라우팅 직전 제어

--- src/router/index.js
+++ src/router/index.js
@@ -10,17 +10,25 @@ import NotFound from '../components/NotFound.vue'
 // 0. lib 사용 설정
 Vue.use(VueRouter)
 
+
+const requireAuth = (to, from, next) => {
+    const isAuth = localStorage.getItem('token')
+    // query string 은 encoding(ASCI 문자열로 변환) 해줘야 하는 것!
+    const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`
+    isAuth ? next() : next(loginPath) // true : routing 로직을 계속 수행
+}
+
 // 3. routes 옵션을 이 곳에서 설정하면서 router 인스턴스를 만듦.
 const router = new VueRouter({
     // 실행모드
     mode: 'history',
     // 2. 라우트 정의
     routes: [
-        { path: '/', component: Home },
+        { path: '/', component: Home , beforeEnter: requireAuth },
         { path: '/login', component: Login },
         {
-            path: '/b/:bid', component: Board, children: [
-                { path: 'c/:cid', component: Card }
+            path: '/b/:bid', component: Board, beforeEnter: requireAuth, children: [
+                { path: 'c/:cid', component: Card ,beforeEnter: requireAuth }
             ]
         },
         { path: '/*', component: NotFound },
